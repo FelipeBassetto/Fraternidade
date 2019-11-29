19d0984d694309dcefd9d2286b8ba457a13048ed
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-11-29 15:27:22 -0300

    ajuste ...

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$servidor = 'https://fraternidade-irma.herokuapp.com/';
-$usuario = 'root';
-$senha = '';
-$database = 'fraternidade';
+$servidor = 'mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$usuario = 'd3p18trtim23isul';
+$senha = 'zf6cbvbbyn9d70iq';
+$database = 'obk6a4eglons2ijq';
 
 $conexao = mysqli_connect($servidor, $usuario, $senha, $database);
 
