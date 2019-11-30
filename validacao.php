f45ec5f3400e5d3601bd3fc0d7fb641bf9f8bee6
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-11-30 10:25:56 -0300

    teste

--- validacao.php
+++ validacao.php
@@ -9,6 +9,6 @@ $senha_bd = $verifica['senha'];
 if($login_bd == $login && $senha_bd == $senha){
   header("Location:index.php?pagina=pessoas");
 }else{
-  echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');</script>";
-  header("Location:index.php?pagina=login");
+  echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');<?php header('Location:index.php?pagina=login'?></script>";
+
 }
