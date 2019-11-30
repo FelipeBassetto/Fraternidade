28c7906e242ea20668353814af6fecf6e1bf573b
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-11-30 10:18:43 -0300

    teste

--- validacao.php
+++ validacao.php
@@ -1,18 +1,13 @@
 <?php
 include db.php;
 $login =(string) $_POST['user'];
-$senha =(string) $_POST['password'];
+$senha =(string) md5($_POST['password']);
 echo $login;
 echo $senha;
 
-$verifica = mysqli_query($conexao,"SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha';") ;
-$rowcount = mysqli_num_rows($verifica);
-echo $rowcount;
-if (mysqli_num_rows($verifica)<=0){
-          echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.html';</script>";
-          die();
-        }else{
-          setcookie("login",$login);
-          header("Location:index.php?pagina=pessoas");
-        }
-?>
+$verifica = mysqli_fetch_array($consulta_usuario) ;
+$login_bd = $verifica['login'];
+$senha_bd = $verifica['senha'];
+if($login_bd == $login && $senha_bd == $senha){
+  
+}
