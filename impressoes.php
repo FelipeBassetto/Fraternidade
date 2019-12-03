d9a50c74e595dc1381e72d9690367c9e4ddf0415
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-12-02 23:27:22 -0300

    radio

--- impressoes.php
+++ impressoes.php
@@ -1,10 +1,16 @@
 <h1 class='titulo'>Selecione oque deseja imprimir.</h1>
 <form method="post" action="processadependente.php">
     <div class="form-group">
-        <input type="radio" onclick="chamacartao()" id="botaocartao">  Cartões das pessoas.
-        <input type="radio" onclick="chamaetiqueta()" id="botaoetiqueta">  Etiquetas.
-        <input type="radio" onclick="chamasacolinhas()" id="botaosacolinhas">  Lista das Sacolinhas.
-        <input type="radio" onclick="chamapresenca()" id="botaopresenca">  Lista de presença.
+        <input type="radio" onclick="chamacartao()" id="impressao">  Cartões das pessoas.
+    </div>
+    <div class="form-group">
+        <input type="radio" onclick="chamaetiqueta()" id="impressao">  Etiquetas.
+    </div>
+    <div class="form-group">
+        <input type="radio" onclick="chamasacolinhas()" id="impressao">  Lista das Sacolinhas.
+    </div>
+    <div class="form-group">
+        <input type="radio" onclick="chamapresenca()" id="impressao">  Lista de presença.
     </div>
     <input type="submit" class="btn btn-primary" value="Gerar impressão" >
     <br/>
