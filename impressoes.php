2474524f9fd210153b22e770f8d7acf588177424
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-12-02 23:13:19 -0300

    teste radio

--- impressoes.php
+++ impressoes.php
@@ -1,17 +1,17 @@
 <h1 class='titulo'>Selecione oque deseja imprimir.</h1>
 <form method="post" action="processadependente.php">
     <div class="form-group">
-        <input type="checkbox" onclick="chamasacolinhas()" id="botaosacolinhas">  Cartões das pessoas.
+        <input type="radio" onclick="chamacartao()" id="botaocartao">  Cartões das pessoas.
     </div>
     <div class="form-group">
-        <input type="checkbox" onclick="chamaetiqueta()" id="botaoetiqueta">  Etiquetas.
+        <input type="radio" onclick="chamaetiqueta()" id="botaoetiqueta">  Etiquetas.
     </div>
     <div class="form-group">
-        <input type="checkbox" onclick="chamasacolinhas()" id="botaosacolinhas">  Lista das Sacolinhas.
+        <input type="radio" onclick="chamasacolinhas()" id="botaosacolinhas">  Lista das Sacolinhas.
     </div>
     <div class="form-group">
-        <input type="checkbox" onclick="chamasacolinhas()" id="botaosacolinhas">  Lista de presença.
+        <input type="radio" onclick="chamapresenca()" id="botaopresenca">  Lista de presença.
     </div>
-    <input type="submit" class="btn btn-primary" value="Cadastrar Dependente" >
+    <input type="submit" class="btn btn-primary" value="Gerar impressão" >
     <br/>
 </form>
