b4d5e405da74aceda29d4d07e31e6cc3f40dc917
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-12-02 23:02:49 -0300

    tentativa

--- impressoes.php
+++ impressoes.php
@@ -1,6 +1,9 @@
 <h1 class='titulo'>Selecione oque deseja imprimir.</h1>
 <form method="post" action="processadependente.php">
     <div class="form-group">
+        <input type="checkbox" onclick="chamasacolinhas()" id="botaosacolinhas">  Cartões das pessoas.
+    </div>
+    <div class="form-group">
         <input type="checkbox" onclick="chamaetiqueta()" id="botaoetiqueta">  Etiquetas.
     </div>
     <div class="form-group">
@@ -9,7 +12,6 @@
     <div class="form-group">
         <input type="checkbox" onclick="chamasacolinhas()" id="botaosacolinhas">  Lista de presença.
     </div>
-    <div class="form-group">
-        <input type="checkbox" onclick="chamasacolinhas()" id="botaosacolinhas">  Cartões das pessoas.
-    </div>
+    <input type="submit" class="btn btn-primary" value="Cadastrar Dependente" >
+    <br/>
 </form>
