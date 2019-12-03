7d5a06580d8f5ae37edf4698561015529c4f35be
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-12-02 23:25:47 -0300

    radio

--- impressoes.php
+++ impressoes.php
@@ -2,14 +2,8 @@
 <form method="post" action="processadependente.php">
     <div class="form-group">
         <input type="radio" onclick="chamacartao()" id="botaocartao">  Cartões das pessoas.
-    </div>
-    <div class="form-group">
         <input type="radio" onclick="chamaetiqueta()" id="botaoetiqueta">  Etiquetas.
-    </div>
-    <div class="form-group">
         <input type="radio" onclick="chamasacolinhas()" id="botaosacolinhas">  Lista das Sacolinhas.
-    </div>
-    <div class="form-group">
         <input type="radio" onclick="chamapresenca()" id="botaopresenca">  Lista de presença.
     </div>
     <input type="submit" class="btn btn-primary" value="Gerar impressão" >
