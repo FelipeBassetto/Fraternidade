86e3815fbf911fe9eebf4936819ef81ce88618d9
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-12-03 22:08:12 -0300

    teste impressão

--- processoimprimir.js
+++ processoimprimir.js
@@ -1,16 +1,15 @@
-<script>
-    var impressao = document.getElementById(impressao);
-    if (impressao == 'cartao') {
-        <span class="popuptext" id="popup cartao">cartao</span>
-    }
-    else if (impressao == 'etiqueta') {
-        <span class="popuptext" id="popup cartao">etiqueta</span>
-    } 
-    else if (impressao == 'sacolinhas') {
-        <span class="popuptext" id="popup cartao">sacolinhas</span>
-    } 
-    else if (impressao == 'presenca') {
-        <span class="popuptext" id="popup cartao">presenca</span>
-    } 
-</script>
+
+var impressao = document.getElementById(impressao);
+if (impressao == 'cartao') {
+    <span class="popuptext" id="popup cartao">cartao</span>
+}
+else if (impressao == 'etiqueta') {
+    <span class="popuptext" id="popup cartao">etiqueta</span>
+} 
+else if (impressao == 'sacolinhas') {
+    <span class="popuptext" id="popup cartao">sacolinhas</span>
+} 
+else if (impressao == 'presenca') {
+    <span class="popuptext" id="popup cartao">presenca</span>
+} 
 <p>teste</p>
