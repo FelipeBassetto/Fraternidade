fd0d7dfa398271d2954867e44d1507bd53a04d22
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-12-03 22:20:14 -0300

    teste impressão

---
+++ processoimprimir.php
@@ -0,0 +1,22 @@
+<?php
+$impressao = $_POST(name);
+if (impressao == 'cartao') {
+    ?>
+    <p>cartao</p>
+    <?php
+}
+else if (impressao == 'etiqueta') {
+    ?>
+    <p>etiqueta</p>
+    <?php
+} 
+else if (impressao == 'sacolinhas') {
+    ?>
+    <p>sacolinhas</p>
+    <?php
+} 
+else if (impressao == 'presenca') {
+    ?>
+    <p>presenca</p>
+    <?php
+} 
