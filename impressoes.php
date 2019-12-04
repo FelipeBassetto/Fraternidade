b0ca5318302fb46ff458fa804712d56cb32ee6f0
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-12-03 22:54:53 -0300

    teste impressão

--- impressoes.php
+++ impressoes.php
@@ -1,16 +1,16 @@
 <h1 class='titulo'>Selecione oque deseja imprimir.</h1>
 <form method="post" action="processoimprimir.js">
     <div class="form-group">
-        <input type="radio" value="cartao" name='impressao' id='impressao' >  Cartões das pessoas.
+        <input type="checkbox" value="cartao" name='impressao' id='impressao' >  Cartões das pessoas.
     </div>
     <div class="form-group">
-        <input type="radio" value="etiqueta" name='impressao' id='impressao'>  Etiquetas.
+        <input type="checkbox" value="etiqueta" name='impressao' id='impressao'>  Etiquetas.
     </div>
     <div class="form-group">
-        <input type="radio" value="sacolinhas" name='impressao' id='impressao'>  Lista das Sacolinhas.
+        <input type="checkbox" value="sacolinhas" name='impressao' id='impressao'>  Lista das Sacolinhas.
     </div>
     <div class="form-group">
-        <input type="radio" value="presenca" name='impressao' id='impressao'>  Lista de presença.
+        <input type="checkbox" value="presenca" name='impressao' id='impressao'>  Lista de presença.
     </div>
     <input type="submit" class="btn btn-primary" value="Gerar impressão" >
     <br/>
