343a1b0c05af5bc1062eb38da6658dbc35d129e0
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-12-06 14:47:22 -0300

    foto fundo login

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 include 'db.php';
-include 'header.php';
+
 
 
 
@@ -13,25 +13,31 @@ else{
 }
 
 if($pagina == 'cadastropessoa'){
+    include 'header.php';
     include 'cadastropessoa.php';
 }
 elseif($pagina == 'pessoas'){
+    include 'header.php';
     include 'pessoas.php';
 }
 elseif($pagina =='login'){
     include 'login.php';
 }
 elseif($pagina == 'cadastrodependente'){
+    include 'header.php';
     include 'cadastrodependente.php';
 }
 elseif($pagina == 'impressoes'){
+    include 'header.php';
     include 'impressoes.php';
 }
 elseif($pagina == 'editarpessoa'){
+    include 'header.php';
     include 'editarpessoa.php';
     include 'consulta_dependentes.php';
 }
 elseif($pagina == 'editardependente'){
+    include 'header.php';
     include 'editardependente.php';
 }
 else{
--- login.php
+++ login.php
@@ -1,9 +1,12 @@
 
 <link rel="stylesheet" type="text/css" media="screen" href="css/login.css">
-
+<link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" media="screen" href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 <div class="row">
     <div class="col-md-7 homeEsquerda">
-        <img src="img/familia.jpg" alt="logo da fraternidade imma maria">
+        <img src="img/logo_fundo.jpeg" alt="logo da fraternidade imma maria">
     </div>
     <div class="col-md-5 homeDireita">
         <form method="post" action="validacao.php">
