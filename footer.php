a2f3ee4ecfab60f81a92fefc34b8d9d7a5034dbd
Felipe Bassetto <felipe.bassetto@hotmail.com>
2019-12-06 15:46:52 -0300

    foto fundo login

--- footer.php
+++ footer.php
@@ -1,8 +1,14 @@
 
 <script src="js/jquery.js"></script>
 <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
 <script>
     $('#pessoas').DataTable();
     $('#dependentes').DataTable();
 </script>
+<footer>
+    <div class="container">
+        <p>©Fraternidade Espírita Irmã Maria</p>
+    </div>
+</footer>
 
