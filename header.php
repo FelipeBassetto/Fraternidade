c54274bed96f9945d8e943b0613b06fc424efad3
Felipe Bassetto <felipe.bassetto@hotmail.com>
2020-01-20 14:54:19 -0300

    ajuste

--- header.php
+++ header.php
@@ -14,10 +14,7 @@
     <header>
         <div class="container">
             <a href="?pagina=<?php echo md5('pessoas')?>"><img src="img/logo.png" alt="logo" title="logo" style="width:6%;"></a>
-            <a href="?pagina=<?php echo md5('impressoes')?>" class="btn"  style="
-    background-color: #0008af;
-    color: #FFF;
-">Impressões</a><p></p>
+            <a href="?pagina=<?php echo md5('impressoes')?>" class="btn">Impressões</a><p></p>
         </div>
     </header>
     <div id="conteudo" class="container">
