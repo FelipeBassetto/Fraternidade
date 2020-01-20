2341217125782a323f7dfe89a12071181ca886a8
Felipe Bassetto <felipe.bassetto@hotmail.com>
2020-01-20 15:02:25 -0300

    teste

--- header.php
+++ header.php
@@ -9,13 +9,18 @@
     <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" media="screen" href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 <body>
     <header>
         <div class="container">
             <a href="?pagina=<?php echo md5('pessoas')?>"><img src="img/logo.png" alt="logo" title="logo" style="width:6%;"></a>
-            <a href="?pagina=<?php echo md5('impressoes')?>" class="botao">Impressões</a><p></p>
+            <a href="?pagina=<?php echo md5('impressoes')?>" class="btn"  style="
+    background-color: #00083F;
+    margin-left: 2%;
+    border-width: 0.5px;
+    border-color: black;
+    color: #FFF;
+">Impressões</a><p></p>
         </div>
     </header>
     <div id="conteudo" class="container">
