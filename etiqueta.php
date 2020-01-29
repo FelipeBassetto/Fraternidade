bbdae15e4880156b0aeb0800b80927e66fc3a591
Guilherme <gui.farias@outlook.com.br>
2020-01-29 10:18:23 -0300

    teste etiqueta

--- etiqueta.php
+++ etiqueta.php
@@ -26,26 +26,66 @@
             <div>
                 <p class="p-lista">Entrega até <?php echo $_POST['data_entregar'];?></p>
             </div>
-            <p class="p-lista">Responsável:</p>
-            <p class="p-lista"><?php echo $linha2['nome']; ?></p>
-            <p class="p-lista">nome:</p>
-            <p class="p-lista"><?php echo $linha['nome_dependente']; ?></p>
-            <p class="p-lista">Sexo:</p>
-            <p class="p-lista"><?php echo $linha['sexo_dependente']; ?></p>
-            <p class="p-lista">Idade:</p>
-            <p class="p-lista"><?php echo $linha['idade_C']; ?></p>
-            <p class="p-lista">Nº Roupa:</p>
-            <p class="p-lista"><?php echo $linha['tamanho_roupa']; ?></p>
-            <p class="p-lista">Calçado:</p>
-            <p class="p-lista"><?php echo $linha['tamanho_sapato']; ?></p>
-            <p class="p-lista">Brinquedo:</p>
-            <p class="p-lista">Novo</p>
-            <p class="p-lista">Necessidades Especiais:</p>
-            <p class="p-lista"><?php echo $linha['deficiencia']; ?></p>
-            <p class="p-lista">Fralda:</p>
-            <p class="p-lista"><?php echo $linha['fralda']; ?></p>
-            <p class="p-lista">Caixa de bombom Nestlê, Garoto ou Lacta.</p>
-            <p class="p-lista">obs: Incluir roupa intíma e embalar todos os presentes.</p>
+            <div>
+                <p class="p-lista">Responsável:</p>
+            </div>
+            <div>
+                <p class="p-lista"><?php echo $linha2['nome']; ?></p>
+            </div>
+            <div>
+                <p class="p-lista">nome:</p>
+            </div>
+            <div>
+                <p class="p-lista"><?php echo $linha['nome_dependente']; ?></p>
+            </div>
+            <div>
+                <p class="p-lista">Sexo:</p>
+            </div>
+            <div>
+                <p class="p-lista"><?php echo $linha['sexo_dependente']; ?></p>
+            </div>
+            <div>
+                <p class="p-lista">Idade:</p>
+            </div>
+            <div>
+                <p class="p-lista"><?php echo $linha['idade_C']; ?></p>
+            </div>
+            <div>
+                <p class="p-lista">Nº Roupa:</p>
+            </div>
+            <div>
+                <p class="p-lista"><?php echo $linha['tamanho_roupa']; ?></p>
+            </div>
+            <div>
+                <p class="p-lista">Calçado:</p>
+            </div>
+            <div>
+                <p class="p-lista"><?php echo $linha['tamanho_sapato']; ?></p>
+            </div>
+            <div>
+                <p class="p-lista">Brinquedo:</p>
+            </div>
+            <div>
+                <p class="p-lista">Novo</p>
+            </div>
+            <div>
+                <p class="p-lista">Necessidades Especiais:</p>
+            </div>
+            <div>
+                <p class="p-lista"><?php echo $linha['deficiencia']; ?></p>
+            </div>
+            <div>
+                <p class="p-lista">Fralda:</p>
+            </div>
+            <div>
+                <p class="p-lista"><?php echo $linha['fralda']; ?></p>
+            </div>
+            <div>
+                <p class="p-lista">Caixa de bombom Nestlê, Garoto ou Lacta.</p>
+            </div>
+            <div>
+                <p class="p-lista">obs: Incluir roupa intíma e embalar todos os presentes.</p>
+            </div>
         </div>
         <?php
                 $conter = $conter +1;
