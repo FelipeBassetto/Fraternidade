aad74b41fe03df6a998596b094490ad3040e9121
Felipe Bassetto <felipe.bassetto@hotmail.com>
2020-02-19 13:09:34 -0300

    teste

--- etiqueta.php
+++ etiqueta.php
@@ -116,89 +116,89 @@
                 ?>
             <div class="table-etiqueta">
                 <div class="div-display">
-                    <div class="p-lista">
+                    <div class="p-lista background-cadastro">
                         <p>Cadastro:<?php echo $linha['num_cadastro']; ?></p>
                     </div>
-                    <div class="p-lista">
+                    <div class="p-lista background-data">
                         <p>Entrega até <?php echo $_POST['data_entregar'];?></p>
                     </div>
                 </div>
                 <div class="div-display">
-                    <div class="p-lista">
+                    <div class="p-lista background-responsavel">
                         <p>Responsável:</p>
                     </div>
-                    <div class="p-lista">
+                    <div class="p-lista background-nomeres">
                         <p><?php echo $linha2['nome']; ?></p>
                     </div>
                 </div>
                 <div class="div-display">
-                    <div class="p-lista">
+                    <div class="p-lista background-nome">
                         <p>nome:</p>
                     </div>
-                    <div class="p-lista">
+                    <div class="p-lista background-crianca">
                         <p><?php echo $linha['nome_dependente']; ?></p>
                     </div>
                 </div>
                 <div class="div-display">
-                    <div class="p-lista">
+                    <div class="p-lista background-sexo">
                         <p>Sexo:</p>
                     </div>
-                    <div class="p-lista">
+                    <div class="p-lista background-MF">
                         <p><?php echo $linha['sexo_dependente']; ?></p>
                     </div>
                 </div>
                 <div class="div-display">
-                    <div class="p-lista">
+                    <div class="p-lista background-idade">
                         <p>Idade:</p>
                     </div>
-                    <div class="p-lista">
+                    <div class="p-lista background-num">
                         <p><?php echo $linha['idade_C']; ?></p>
                     </div>
                 </div>
                 <div class="div-display">
-                    <div class="p-lista">
+                    <div class="p-lista background-roupa">
                         <p>Nº Roupa:</p>
                     </div>
-                    <div class="p-lista">
+                    <div class="p-lista background-tamanhoroupa">
                         <p><?php echo $linha['tamanho_roupa']; ?></p>
                     </div>
                 </div>
                 <div class="div-display">
-                    <div class="p-lista">
+                    <div class="p-lista background-sapato">
                         <p>Calçado:</p>
                     </div>
-                    <div class="p-lista">
+                    <div class="p-lista background-tamanhosapato">
                         <p><?php echo $linha['tamanho_sapato']; ?></p>
                     </div>
                 </div>
                 <div class="div-display">
-                    <div class="p-lista">
+                    <div class="p-lista background-brinquedo">
                         <p>Brinquedo:</p>
                     </div>
-                    <div class="p-lista">
+                    <div class="p-lista background-estado">
                         <p>Novo</p>
                     </div>
                 </div>
                 <div class="div-display">
-                    <div class="p-lista">
+                    <div class="p-lista background-nescessidade">
                         <p>Necessidades Especiais:</p>
                     </div>
-                    <div class="p-lista">
+                    <div class="p-lista background-especiais">
                         <p><?php echo $linha['deficiencia']; ?></p>
                     </div>
                 </div>
                 <div class="div-display">
-                    <div class="p-lista">
+                    <div class="p-lista background-fralda">
                         <p>Fralda:</p>
                     </div>
-                    <div class="p-lista">
+                    <div class="p-lista background-tamanhofralda">
                         <p><?php echo $linha['fralda']; ?></p>
                     </div>
                 </div>
-                <div class="p-lista">
+                <div class="p-lista caixabombom">
                     <p>Caixa de bombom Nestlê, Garoto ou Lacta.</p>
                 </div>
-                <div class="p-lista">
+                <div class="p-lista obs">
                     <p>obs: Incluir roupa intíma e embalar todos os presentes.</p>
                 </div>
             </div>
